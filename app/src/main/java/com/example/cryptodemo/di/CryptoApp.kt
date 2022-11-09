759353b452aef50673cc2635739b63327309990f
mingaleev.d92@mail.ru <tatarstan1204BTc)>
2022-11-09 16:27:24 +0300

    создани module

---
+++ app/src/main/java/com/example/cryptodemo/di/CryptoApp.kt
@@ -0,0 +1,12 @@
+package com.example.cryptodemo.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author : Mingaleev D
+ * @data : 9/11/2022
+ */
+@HiltAndroidApp
+class CryptoApp:Application() {
+}
