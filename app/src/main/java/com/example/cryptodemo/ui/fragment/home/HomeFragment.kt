032410e10611c68152d0aaa4e43acf9e91488faf
mingaleev.d92@mail.ru <tatarstan1204BTc)>
2022-11-10 21:38:20 +0300

    адаптер HomeFragment.kt and (ripple rv)

--- app/src/main/java/com/example/cryptodemo/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/cryptodemo/ui/fragment/home/HomeFragment.kt
@@ -1,8 +1,13 @@
 package com.example.cryptodemo.ui.fragment.home
 
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.example.cryptodemo.base.BaseFragment
 import com.example.cryptodemo.databinding.FragmentHomeBinding
+import com.example.cryptodemo.model.successfulResponse.Data
+import com.example.cryptodemo.ui.fragment.adapter.HomeAdapter
+import com.example.cryptodemo.ui.fragment.adapter.ItemClickListener
 import com.example.cryptodemo.utils.Constants.API_KEY
 import com.example.cryptodemo.utils.Constants.LIMIT
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,7 +27,35 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
    }
 
    override fun observerEvents() {
-     // TODO("Not yet implemented")
+      with(viewModel) {
+         cryptoResponse.observe(viewLifecycleOwner) { cryptoResponse ->
+            cryptoResponse?.let {
+               it.data?.let { it1 -> setRecycler(it1) }
+            }
+         }
+         isLoading.observe(viewLifecycleOwner) {
+            handleViews(it)
+         }
+         onError.observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), "Error $it", Toast.LENGTH_SHORT).show()
+         }
+      }
+   }
+
+   private fun setRecycler(data: List<Data>) {
+      val mAdapter = HomeAdapter(object : ItemClickListener {
+         override fun onItemClick(coin: Data) {
+
+         }
+
+      })
+      binding.homeRv.adapter = mAdapter
+      mAdapter.setList(data)
+   }
+
+   private fun handleViews(isLoading: Boolean = false) {
+      binding.homeRv.isVisible = !isLoading
+      binding.progressBar.isVisible = isLoading
    }
 
 }
